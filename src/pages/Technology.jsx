158583495a3c1d094638e453a1e8872e01a3d918
Nicholas <nickmanic@hotmail.com>
2025-12-15 19:28:37 +1100

    feat: enhance Technology page with responsive images and updated styling

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,6 +1,9 @@
 import spaceport from '../assets/technology/image-spaceport-portrait.jpg';
 import capsule from '../assets/technology/image-space-capsule-portrait.jpg';
 import vehicle from '../assets/technology/image-launch-vehicle-portrait.jpg';
+import spaceportLandscape from '../assets/technology/image-spaceport-landscape.jpg';
+import capsuleLandscape from '../assets/technology/image-space-capsule-landscape.jpg';
+import vehicleLandscape from '../assets/technology/image-launch-vehicle-landscape.jpg';
 import classes from './Technology.module.css';
 import { useState } from 'react';
 
@@ -8,22 +11,25 @@ const technology_info = [
   {
     name: 'LAUNCH VEHICLE',
     description:
-      'A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth\'s surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it\'s quite an awe-inspiring sight on the launch pad!',
+      "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
     image: vehicle,
+    imageLandscape: vehicleLandscape,
   },
   {
     name: 'SPACEPORT',
     description:
       'A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.',
     image: spaceport,
+    imageLandscape: spaceportLandscape,
   },
   {
     name: 'SPACE CAPSULE',
     description:
       'A space capsule is a type of spacecraft that is designed to return to Earth in a controlled manner. Our space capsule is equipped with advanced heat shields and parachutes to ensure a safe landing.',
     image: capsule,
-  }
-]
+    imageLandscape: capsuleLandscape,
+  },
+];
 
 export const Technology = () => {
   const [currentTech, setCurrentTech] = useState('LAUNCH VEHICLE');
@@ -40,8 +46,9 @@ export const Technology = () => {
             return (
               <button
                 key={tech.name}
-                className={`${classes.btn} font-barlow-preset-8 ${currentTech === tech.name ? classes.active : ''
-                  }`}
+                className={`${classes.btn} font-bellefair-preset-4 ${
+                  currentTech === tech.name ? classes.active : ''
+                }`}
                 onClick={() => setCurrentTech(tech.name)}
               >
                 <p>{index + 1}</p>
@@ -49,16 +56,33 @@ export const Technology = () => {
             );
           })}
         </div>
-
-        <h2 className={`${classes.technologyName} font-barlow-preset-5`}>
-          {tech.name}
-        </h2>
-        <p className={`${classes.technologyDescription} font-barlow-preset-6`}>
-          {tech.description}
-        </p>
-        <img src={tech.image} className={classes.techImage} alt="Technology" />
-
+        <div className={classes.descriptionContainer}>
+          <p style={{ color: '#ffffff40' }} className="font-bellefair-preset-4">
+            THE TERMINOLOGY...
+          </p>
+          <p className={`${classes.technologyName} font-bellefair-preset-3`}>
+            {tech.name}
+          </p>
+          <p
+            className={`${classes.technologyDescription} font-barlow-preset-9`}
+          >
+            {tech.description}
+          </p>
+        </div>
+        <div className={classes.imageContainer}>
+          <picture>
+            <source
+              media="(max-width:1024px)"
+              srcset={tech.imageLandscape}
+            ></source>
+            <img
+              src={tech.image}
+              className={classes.techImage}
+              alt="Technology"
+            />
+          </picture>
+        </div>
       </div>
     </div>
   );
-}
+};
